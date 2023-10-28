8c73e7e13128ee476425abdb10995b596bc0c883
caguida <calebcarloss@gmail.com>
2023-10-28 14:32:43 +0200

    fisrt commit

---
+++ sett.py
@@ -0,0 +1,24 @@
+token = 'bigdateros'
+
+whatsapp_token = 'EAAEpadzUgZCkBO5YPfM7xlCjH4WloLdnuHw3Ghb0MJZA0IOHiUJYSt3Yb7IgxWELiyKIycKyjiFHsspgzXAnuSFecMUQ9SHE4owbuZA8p9iF2MLmbiOkcyVRuEC03q679Dp9PRhmtJFNuEW88lcdE2bTWoAf18MapbEHc9DSUVtrv22GJWhAGWqq1quZBz7hcsruMdZAV9wtBspw8VP1D1548b2jGEOcZAZAsuiSJgZD'
+whatsapp_url = 'https://graph.facebook.com/v17.0/156524200875526/messages'
+
+stickers = {
+    "poyo_feliz": 984778742532668,
+    "perro_traje": 1009219236749949,
+    "perro_triste": 982264672785815,
+    "pedro_pascal_love": 801721017874258,
+    "pelfet": 3127736384038169,
+    "anotado": 24039533498978939,
+    "gato_festejando": 1736736493414401,
+    "okis": 268811655677102,
+    "cachetada": 275511571531644,
+    "gato_juzgando": 107235069063072,
+    "chicorita": 3431648470417135,
+    "gato_triste": 210492141865964,
+    "gato_cansado": 1021308728970759
+}
+
+document_url = "https://www.africau.edu/images/default/sample.pdf"
+
+openai_key = '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'
